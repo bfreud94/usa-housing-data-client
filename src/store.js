fd54b70ea691938ea14d4c349ecd37d043ef3dc5
Ben Freudman <bfreud94@gmail.com>
2020-10-27 11:47:58 -0400

    Changed Favicon; updated store for other browsers

--- src/store.js
+++ src/store.js
@@ -9,9 +9,13 @@ const middleware = [thunk];
 const store = createStore(
     rootReducer,
     initialState,
-    compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()
+    process.env.NODE_ENV.trim() === 'development'
+    ? compose(
+            applyMiddleware(...middleware),
+            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+    : compose(
+        applyMiddleware(...middleware)
     )
 );
 
